03ae67c527faff20f8388209e08f2a4a8b85e2f9
LAPTOP-OK9FJSJC\CJ0201010 <tom70215@yahoo.com.tw>
2021-02-21 12:41:32 +0800

    3nd Version

--- Gitxx/src/testApp/MainApp.java
+++ Gitxx/src/testApp/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2nd Version");
+		System.out.println("3nd Version");
 
 	}
 
